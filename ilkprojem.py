ac83a0419b26efd4058fc088092b5c3544000c75
huseyinsuo <huseyinnaslan18@gmail.com>
2023-08-20 17:27:39 +0300

    ilk projem

--- ilkprojem.py
+++ ilkprojem.py
@@ -1,7 +1,4 @@
 yil = int(input("dogum yilinizi giriniz :"))
 yas = (2023 - yil)
 print("yasiniz :" , yas)
-if yas < 18 : 
-    print("Henüz yetişkin değilsiniz.")
-else:
-    print("Yetişkinsiniz.")
+
